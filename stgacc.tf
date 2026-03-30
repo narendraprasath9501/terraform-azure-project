c4adb5e43310cac5e70b2a79540b69b658d32996
Narendraprasath <narendraprasath9501@gmail.com>
2026-03-30 13:37:24 +0530

    Added tags to storage account

--- stgacc.tf
+++ stgacc.tf
@@ -6,3 +6,8 @@ resource "azurerm_storage_account" "sa" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+tags = {
+  environment = "dev"
+  owner       = "naren"
+}
