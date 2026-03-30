e9690abd03ed2b0cffbe5882d4bd048784c16677
Narendraprasath <narendraprasath9501@gmail.com>
2026-03-30 14:14:43 +0530

    restored clean storage account file from stable commit

--- stgacc.tf
+++ stgacc.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "sa" {
-  name                = "tfstorenaren123"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = "Central India"
+  name                     = "tfstorenaren123"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = "Central India"
 
   account_tier             = "Standard"
   account_replication_type = "LRS"
