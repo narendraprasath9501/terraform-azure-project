0a1cd58a90f549fe5a2097b662d29aa459525f2b
Narendraprasath <narendraprasath9501@gmail.com>
2026-03-30 14:08:05 +0530

    fixed storage account structure

--- stgacc.tf
+++ stgacc.tf
@@ -1,13 +1,8 @@
 resource "azurerm_storage_account" "sa" {
-  name                     = "tfstorenaren123"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = "Central India"
+  name                = "tfstorenaren123"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = "Central India"
 
   account_tier             = "Standard"
   account_replication_type = "LRS"
-}
-
-tags = {
-  environment = "dev"
-  owner       = "naren"
 }
